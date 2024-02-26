87fe4efba2a30ce5781928ea4f9bbcc8f1d9b1fe
adityjha0 <aditya2005aps@gmail.com>
2024-02-26 20:07:09 +0530

    fib with dp

--- fib.rs
+++ fib.rs
@@ -1,11 +1,26 @@
 use std::io;
 
-fn fib(x:i64)->i64{
+// fn fibrec(x:i64)->i64{
+//     if x==0 || x==1{
+//         x
+//     }else{
+//         fibrec(x-1)+fibrec(x-2)
+//     }
+// }
+
+fn fibdp(x:i64)->i64{
     if x==0 || x==1{
-        x
-    }else{
-        fib(x-1)+fib(x-2)
+        return x;
     }
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 1;
+    for _i in 2..(x+1) {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    c
 }
 
 fn main(){
@@ -13,5 +28,5 @@ fn main(){
     io::stdin().read_line(&mut x).expect("couldn't read line");
     let n:i64 = x.trim().parse().expect("please enter a number");
 
-    println!("The {}th fibonacci is {}",n,fib(n));    
+    println!("The {}th fibonacci is {}",n,fibdp(n));    
 }
