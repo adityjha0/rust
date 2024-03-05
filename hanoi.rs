7e738dd4a5776c9fb3ec3aa779120968195bdcba
adityjha0 <aditya2005aps@gmail.com>
2024-03-06 03:05:39 +0530

    corrected algorithm

--- hanoi.rs
+++ hanoi.rs
@@ -8,23 +8,11 @@ fn main(){
 }
 
 fn hanoi(n:i32, s:i32, m:i32, e:i32){
-    if n==1 {
-        println!("{} to {}",s,e);
-    }else if n==2 {
-        println!("{} to {}",s,m);
-        println!("{} to {}",s,e);
-        println!("{} to {}",m,e);
-    }else if n==3 {
-        println!("{} to {}",s,e);
-        println!("{} to {}",s,m);
-        println!("{} to {}",e,m);
-        println!("{} to {}",s,e);
-        println!("{} to {}",m,s);
-        println!("{} to {}",m,e);
-        println!("{} to {}",s,e);
+    if n==0 {
+        return;
     }else{
-        hanoi(n-1, s, m, e);
+        hanoi(n-1, s, e, m);
         println!("{} to {}",s,e);
-        hanoi(n-1, m, e, s);
+        hanoi(n-1, m, s, e);
     }
 }
